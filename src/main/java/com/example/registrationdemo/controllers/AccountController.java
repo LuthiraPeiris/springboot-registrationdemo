56afdcbbb711423c612140339b2983770508536c
Luthira Peiris <22cse0369@ms.sab.ac.lk>
2025-11-27 19:06:25 +0530

    Second commit

--- src/main/java/com/example/registrationdemo/controllers/AccountController.java
+++ src/main/java/com/example/registrationdemo/controllers/AccountController.java
@@ -1,11 +1,20 @@
 package com.example.registrationdemo.controllers;
 
+import com.example.registrationdemo.models.AppUser;
 import com.example.registrationdemo.models.RegisterDto;
 import com.example.registrationdemo.repo.AppUserRepo;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Date;
 
 @Controller
 public class AccountController {
@@ -17,6 +26,59 @@ public class AccountController {
     public String register(Model model) {
         RegisterDto registerDto = new RegisterDto();
         model.addAttribute(registerDto);
+        model.addAttribute("success", false);
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register (
+            Model model,
+            @Valid @ModelAttribute RegisterDto registerDto,
+            BindingResult result
+    ) {
+
+        if (!registerDto.getPassword().equals(registerDto.getConfirmPassword())) {
+            result.addError(
+                    new FieldError("registerDto", "confirmPassword", "Password and Confirm Password do not match")
+            );
+        }
+
+        AppUser appUser = repo.findByEmail(registerDto.getEmail());
+        if(appUser != null) {
+            result.addError(
+                    new FieldError("registerDto", "email", "Email address is already used")
+            );
+        }
+
+        if (result.hasErrors()) {
+            return "register";
+        }
+
+        try {
+            // create new account
+            var bCryptEncoder = new BCryptPasswordEncoder();
+
+            AppUser newUser = new AppUser();
+            newUser.setFirstName(registerDto.getFirstName());
+            newUser.setLastName(registerDto.getLastName());
+            newUser.setEmail(registerDto.getEmail());
+            newUser.setPhone(registerDto.getPhone());
+            newUser.setAddress(registerDto.getAddress());
+            newUser.setRole("client");
+            newUser.setCreatedAt(new Date());
+            newUser.setPassword(bCryptEncoder.encode(registerDto.getPassword()));
+
+            repo.save(newUser);
+
+            model.addAttribute("registerDto", new RegisterDto());
+            model.addAttribute("success", true);
+
+        } catch (Exception e) {
+            result.addError(
+                    new FieldError("registerDto", "firstName", e.getMessage())
+            );
+        }
+
         return "register";
     }
 }
